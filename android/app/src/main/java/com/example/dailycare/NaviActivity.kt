442ba4bb6f9aa548b0c0ef0746d86849a9ff54f7
SeongWooHong <pluto0303@naver.com>
2023-10-29 09:56:30 +0900

    Add navigation bar xml and res

---
+++ android/app/src/main/java/com/example/dailycare/NaviActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dailycare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NaviActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navi)
+    }
+}
