7c9235208eccbe2905024cf7df0b1185d3e5f299
SeongWooHong <pluto0303@naver.com>
2023-11-02 15:01:30 +0900

    chore: add icon

--- android/app/src/main/java/com/example/dailycare/NaviActivity.kt
+++ android/app/src/main/java/com/example/dailycare/NaviActivity.kt
@@ -22,7 +22,6 @@ class NaviActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
-
     }
 
 }
